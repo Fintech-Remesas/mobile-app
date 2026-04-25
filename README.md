8a49e91827b4c399fa3e79cc8d1a5d914b7e4b39
Daniel Del Castillo <u202211212@upc.edu.pe>
2026-04-25 17:32:36 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sagiro_mobile_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/pe/edu/upc/sagiro_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package pe.edu.upc.sagiro_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
